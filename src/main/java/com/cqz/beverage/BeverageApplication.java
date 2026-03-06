e5232ef62507a60166f3ee1994907622390ea6d0
zhanglinshuai <1216486490@qq.com>
2026-03-06 10:11:01 +0800

    创建项目

---
+++ src/main/java/com/cqz/beverage/BeverageApplication.java
@@ -0,0 +1,13 @@
+package com.cqz.beverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeverageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqz/beverage/BeverageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqz.beverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
